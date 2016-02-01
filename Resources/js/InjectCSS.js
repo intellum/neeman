3e968acae5397d7b33d409fc0c26da610c1ba9b0
Stephen Williams <stephen@onato.com>
2016-02-01 19:37:18 +1300

    Switch HTML tag to one less likely to be used by another library.

--- Resources/js/InjectCSS.js
+++ Resources/js/InjectCSS.js
@@ -1,7 +1,7 @@
 document.documentElement.style.webkitTouchCallout='none';
 
 var root = document.getElementsByTagName( 'html' )[0]
-root.setAttribute( 'class', 'hybrid' );
+root.setAttribute( 'class', 'neeman-hybrid-app' );
 
 var styleElement = document.createElement('style');
 root.appendChild(styleElement);
