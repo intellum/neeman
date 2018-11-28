2b2ab0b1c6ced855645eb59c636b8cafc512d751
Rob Nash <robscode@icloud.com>
2018-11-28 15:23:42 +0000

    Remove older swift syntax.

--- Examples/Intermediate/Github/AppDelegate.swift
+++ Examples/Intermediate/Github/AppDelegate.swift
@@ -10,30 +10,5 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
-
-    func application(application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [NSObject: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
-
-    func applicationWillResignActive(application: UIApplication) {
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-    }
-
-
 }
